71a5371d5d7f36dc0d020c21642a27a8c4e3923f
Ivano <ivano.vingiani@gmail.com>
2017-06-12 22:49:20 +0100

    Completed getEvents()

--- eventweather/src/main/java/org/ivavin/eventweather/service/EventfulEventService.java
+++ eventweather/src/main/java/org/ivavin/eventweather/service/EventfulEventService.java
@@ -7,9 +7,12 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.ivavin.eventweather.model.Event;
 import org.ivavin.eventweather.model.EventResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,73 +20,84 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 /**
  * Implementation of EventService which uses the EventFul API.
  * http://api.eventful.com/
- * 
+ *
  * @author Ivano
- * 
+ *
  */
+@Service
 public class EventfulEventService implements EventService {
 
-  private final RestTemplate restTemplate;
-  private final String baseURL;
+	private final static String API_URL = "http://api.eventful.com/json/events/search";
 
-  /**
-   * @param baseURL
-   *          the API's base URL
-   */
-  public EventfulEventService(final String newBaseURL) {
-    baseURL = newBaseURL;
-    restTemplate = new RestTemplate();
-    initConvertersForEventful(restTemplate);
-  }
+	private final RestTemplate restTemplate;
+	private final String appKey;
 
-  public List<Event> getEvents(final String location, final String category, final String... queryParams) {
-    EventResponse eventResponse = restTemplate.getForObject(buildRequestURL(location, category, queryParams),
-        EventResponse.class);
+	/**
+	 * @param appKey
+	 *            the API's authentication key
+	 */
+	@Autowired
+	public EventfulEventService(final Environment env) {
+		appKey = env.getProperty("eventful.app_key");
+		restTemplate = new RestTemplate();
+		initConvertersForEventful(restTemplate);
+	}
 
-    List<Event> events = eventResponse.getEvents().getEvent();
-    if (StringUtils.isNotBlank(category)) {
-      setCategory(events, category);
-    }
-    return events;
-  }
+	@Override
+	public List<Event> getEvents(final String location, final String category, final String... queryParams) {
+		EventResponse eventResponse = restTemplate.getForObject(buildRequestURL(location, category, queryParams),
+				EventResponse.class);
 
-  private void setCategory(final List<Event> events, final String category) {
-    for (Event event : events) {
-      event.setCategory(category);
-    }
-  }
+		List<Event> events = eventResponse.getEvents().getEvent();
+		if (StringUtils.isNotBlank(category)) {
+			setCategory(events, category);
+		}
+		return events;
+	}
 
-  private String buildRequestURL(final String location, final String category, final String[] queryParams) {
+	private void setCategory(final List<Event> events, final String category) {
+		for (Event event : events) {
+			event.setCategory(category);
+		}
+	}
 
-    String result = baseURL + "?location=" + location;
+	private String buildRequestURL(final String location, final String category, final String[] queryParams) {
 
-    if (StringUtils.isNotBlank(category)) {
-      result += "&category=" + category;
-    }
+		String result = API_URL + "?app_key=" + appKey + "&location=" + location;
 
-    /* Extra query patams ignored for now */
+		if (StringUtils.isNotBlank(category)) {
+			result += "&category=" + category;
+		}
 
-    return result;
-  }
+		/* Extra query params ignored for now */
 
-  /**
-   * EventFul returns the wrong MIME type ("text/javascript" instead of
-   * "application/json"), so we need to adjust the converter's mapping.
-   * 
-   * @param restTemplate
-   */
-  private static void initConvertersForEventful(final RestTemplate restTemplate) {
-    List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
-    List<MediaType> jsonMediaTypes = Arrays.asList(new MediaType("application", "json",
-        MappingJackson2HttpMessageConverter.DEFAULT_CHARSET), new MediaType("text", "javascript",
-        MappingJackson2HttpMessageConverter.DEFAULT_CHARSET));
+		return result;
+	}
 
-    for (HttpMessageConverter<?> converter : converters) {
-      if (converter instanceof MappingJackson2HttpMessageConverter) {
-        MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
-        jsonConverter.setObjectMapper(new ObjectMapper());
-        jsonConverter.setSupportedMediaTypes(Collections.unmodifiableList(jsonMediaTypes));
-      }
-    }
-  }
+	/**
+	 * EventFul returns the wrong MIME type ("text/javascript" instead of
+	 * "application/json"), so we need to adjust the converter's mapping.
+	 *
+	 * @param restTemplate
+	 */
+	private static void initConvertersForEventful(final RestTemplate restTemplate) {
+		List<HttpMessageConverter<?>> converters = restTemplate.getMessageConverters();
+		List<MediaType> jsonMediaTypes = Arrays.asList(
+				new MediaType("application", "json", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET),
+				new MediaType("text", "javascript", MappingJackson2HttpMessageConverter.DEFAULT_CHARSET));
+
+		for (HttpMessageConverter<?> converter : converters) {
+			if (converter instanceof MappingJackson2HttpMessageConverter) {
+				MappingJackson2HttpMessageConverter jsonConverter = (MappingJackson2HttpMessageConverter) converter;
+				jsonConverter.setObjectMapper(new ObjectMapper());
+				jsonConverter.setSupportedMediaTypes(Collections.unmodifiableList(jsonMediaTypes));
+			}
+		}
+	}
+
+	@Override
+	public List<String> getCategories() {
+		// TODO
+		return new ArrayList<String>();
+	}
 }
