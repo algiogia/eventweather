c1dadaf7b1e9b7ac26b775a8d3101797e58b4fab
Ivano <ivano.vingiani@gmail.com>
2017-06-12 17:29:05 +0100

    EventService interface

---
+++ eventweather/src/main/java/org/ivavin/eventweather/service/EventService.java
@@ -0,0 +1,27 @@
+package org.ivavin.eventweather.service;
+
+import java.util.List;
+
+import org.ivavin.eventweather.model.Event;
+
+/**
+ * The EventService connects to an API that provides events information.
+ * 
+ * @author Ivano
+ * 
+ */
+public interface EventService {
+
+  /**
+   * Returns a list of events
+   * 
+   * @param location
+   *          the location for the events, not <code>null</code>
+   * @param category
+   *          the optional category
+   * @param params
+   *          optional search parameters
+   * @return a list of {@link Event}s
+   */
+  List<Event> getEvents(String location, String category, String... queryParams);
+}
