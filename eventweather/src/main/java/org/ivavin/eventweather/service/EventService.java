fa3c93baf90053f3bd796d094b43a9e2d8bae5fe
Ivano <ivano.vingiani@gmail.com>
2017-06-12 22:48:04 +0100

    Added getCategories()

--- eventweather/src/main/java/org/ivavin/eventweather/service/EventService.java
+++ eventweather/src/main/java/org/ivavin/eventweather/service/EventService.java
@@ -6,22 +6,29 @@ import org.ivavin.eventweather.model.Event;
 
 /**
  * The EventService connects to an API that provides events information.
- * 
+ *
  * @author Ivano
- * 
+ *
  */
 public interface EventService {
 
-  /**
-   * Returns a list of events
-   * 
-   * @param location
-   *          the location for the events, not <code>null</code>
-   * @param category
-   *          the optional category
-   * @param params
-   *          optional search parameters
-   * @return a list of {@link Event}s
-   */
-  List<Event> getEvents(String location, String category, String... queryParams);
+	/**
+	 * Returns a list of events
+	 * 
+	 * @param location
+	 *            the location for the events, not <code>null</code>
+	 * @param category
+	 *            the optional category
+	 * @param params
+	 *            optional search parameters
+	 * @return a list of {@link Event}s
+	 */
+	List<Event> getEvents(String location, String category, String... queryParams);
+
+	/**
+	 * Returns the list of available categories
+	 * 
+	 * @return a list of Strings
+	 */
+	List<String> getCategories();
 }
