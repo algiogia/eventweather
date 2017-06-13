b0d841ef2e2a01d90f7b9f20af0f4d367fdd11fb
Ivano <ivano.vingiani@gmail.com>
2017-06-13 09:01:13 +0100

    Hashcode and equals

--- eventweather/src/main/java/org/ivavin/eventweather/model/EventResponse.java
+++ eventweather/src/main/java/org/ivavin/eventweather/model/EventResponse.java
@@ -7,34 +7,73 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EventResponse implements Serializable {
 
-  private static final long serialVersionUID = -7217982303445577381L;
+	private static final long serialVersionUID = -7217982303445577381L;
 
-  private String pageCount;
+	private String pageCount;
 
-  private Events events;
+	private Events events;
 
-  public EventResponse() {
-  }
+	public EventResponse() {
+	}
 
-  public String getPageCount() {
-    return pageCount;
-  }
+	public String getPageCount() {
+		return pageCount;
+	}
 
-  public void setPageCount(String pageCount) {
-    this.pageCount = pageCount;
-  }
+	public void setPageCount(final String pageCount) {
+		this.pageCount = pageCount;
+	}
 
-  public Events getEvents() {
-    return events;
-  }
+	public Events getEvents() {
+		return events;
+	}
 
-  public void setEvents(Events events) {
-    this.events = events;
-  }
+	public void setEvents(final Events events) {
+		this.events = events;
+	}
 
-  @Override
-  public String toString() {
+	@Override
+	public String toString() {
 
-    return "EventRequest{" + "pageCount='" + pageCount + '\'' + ", events=" + events.toString() + '}';
-  }
+		return "EventRequest{" + "pageCount='" + pageCount + '\'' + ", events=" + events.toString() + '}';
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((events == null) ? 0 : events.hashCode());
+		result = prime * result + ((pageCount == null) ? 0 : pageCount.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		EventResponse other = (EventResponse) obj;
+		if (events == null) {
+			if (other.events != null) {
+				return false;
+			}
+		} else if (!events.equals(other.events)) {
+			return false;
+		}
+		if (pageCount == null) {
+			if (other.pageCount != null) {
+				return false;
+			}
+		} else if (!pageCount.equals(other.pageCount)) {
+			return false;
+		}
+		return true;
+	}
+
 }
--- eventweather/src/main/java/org/ivavin/eventweather/model/Events.java
+++ eventweather/src/main/java/org/ivavin/eventweather/model/Events.java
@@ -8,23 +8,54 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Events implements Serializable {
 
-  private static final long serialVersionUID = -7444776712396688485L;
+	private static final long serialVersionUID = -7444776712396688485L;
 
-  private List<Event> event = null;
+	private List<Event> event = null;
 
-  public Events() {
-  }
+	public Events() {
+	}
 
-  public List<Event> getEvent() {
-    return event;
-  }
+	public List<Event> getEvent() {
+		return event;
+	}
 
-  public void setEvent(List<Event> event) {
-    this.event = event;
-  }
+	public void setEvent(final List<Event> event) {
+		this.event = event;
+	}
 
-  @Override
-  public String toString() {
-    return "Events{" + "event='" + event.toString() + '}';
-  }
+	@Override
+	public String toString() {
+		return "Events{" + "event='" + event.toString() + '}';
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((event == null) ? 0 : event.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Events other = (Events) obj;
+		if (event == null) {
+			if (other.event != null) {
+				return false;
+			}
+		} else if (!event.equals(other.event)) {
+			return false;
+		}
+		return true;
+	}
+
 }
